e90bb705cdd0de6b3ebb783fd137b0d42a1df64c
James Dunlop <jamezdunlop@gmail.com>
2024-08-01 09:02:47 +1200

    v3

---
+++ worksheet2/my-nest-app/src/dummydata/artices.ts
@@ -0,0 +1,43 @@
+export const ARTICLES: any[] = [
+    {
+        _id: "1",
+        title: "An experimental evalution of test driven development vs. test-last develpment with industry professionamls",
+        authes: "Munir, h., k.",
+        sources: "Ease",
+        pubyear: "2014",
+        doi: "https://www.youtube.com/",
+        claim_evidence: [["code improvemrnt", "strong support"],["product improvement", "weack against"],["team improvement", "none"]],
+
+    },
+    {
+        _id: "2",
+        title: "Realising quality improvement......",
+        authes: "tom, hoppkins., kendra.",
+        sources: "EA sports",
+        pubyear: "2014",
+        doi: "https://www.youtube.com/shorts/nwJVWVdYtJI",
+        claim_evidence: [["code improvemrnt", "weak support"],["product improvement", "low against"],["team improvement", "none"]],
+
+    },
+    {
+        _id: "3",
+        title: "Dose test driven development......",
+        authes: "jerry, sven., ronny.",
+        sources: "amizon",
+        pubyear: "2014",
+        doi: "https://www.youtube.com/shorts/Rb2jBzLqYrI",
+        claim_evidence: [["code improvemrnt", "medum support"],["product de-provement", "low against"],["team improvement", "none"]],
+
+    },
+    {
+        _id: "4",
+        title: "AComparitive Case studdy on......",
+        authes: "steve, jack., brian.",
+        sources: "mc donalds",
+        pubyear: "2023",
+        doi: "https://www.youtube.com/shorts/qRNzJmMwtzI",
+        claim_evidence: [["code improvemrnt", "medum support"],["product de-provement", "low against"],["team improvement", "none"]],
+
+    },
+
+];
